abeffd1b88e42ad665d97efb097062d5b1c1bcf0
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-07 11:04:36 +0200

    added execution and result models

--- data_analytics_service/analytics/models.py
+++ data_analytics_service/analytics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Execution(models.Model):
+    Execution_tag = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Result(models.Model):
+    execution = models.ForeignKey(Execution, on_delete=models.CASCADE)
+    result_text = models.CharField(max_length=200)
+    start_date = models.DateTimeField('date started')
