433d8e92e8bc55aab5e24a90285e87d0e549fbaa
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-07 11:43:14 +0200

    added date handlers for execution models

--- data_analytics_service/analytics/models.py
+++ data_analytics_service/analytics/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from datetime import datetime, timedelta
 
 
 class Execution(models.Model):
-    Execution_tag = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    execution_tag = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(default=datetime.now())
 
+    def __str__(self):
+        return self.execution_tag
+
+    def was_created_recently(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Result(models.Model):
     execution = models.ForeignKey(Execution, on_delete=models.CASCADE)
     result_text = models.CharField(max_length=200)
     start_date = models.DateTimeField('date started')
+
+    def __str__(self):
+        return self.result_text
