38577db8b243242e4b85795b6c93a272bc831fc7
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-12 16:25:47 +0200

    added views for executions and users

--- data_analytics_service/analytics/views.py
+++ data_analytics_service/analytics/views.py
@@ -1,5 +1,36 @@
 from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from analytics.serializers import UserSerializer, GroupSerializer, ExecutionSerializer
+from .models import Execution
 
 
 def index(request):
-    return HttpResponse("This is the Analytics index.")
+    latest_execution_list = Execution.objects.order_by('creation_date')[:5]
+    output = ', '.join([execution.execution_tag for execution in latest_execution_list])
+    return HttpResponse(output)
+
+class ExecutionViewSet(viewsets.ModelViewSet):
+    """
+    Executions list
+    """
+    queryset = Execution.objects.all()
+    serializer_class = ExecutionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Users list
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Groupds list
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
