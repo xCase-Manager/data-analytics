31d82d1e2b2bf5b6c1a9eb7eee3377232548e3f8
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-12 16:24:41 +0200

    added URL config for executions, users and groups

--- data_analytics_service/analytics/urls.py
+++ data_analytics_service/analytics/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-
+from django.urls import include, path
 from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'executions', views.ExecutionViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
