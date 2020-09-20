fa4fdc9da5b0d275132f44ce7799a0ae81124ecf
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-20 15:52:41 +0200

    changed execution model properties

--- analytics/models.py
+++ analytics/models.py
@@ -6,20 +6,17 @@ from django.utils import timezone
 class Job(models.Model):
     name = models.CharField(max_length=50)
     tags = models.CharField(max_length=200)
-    creation_date = models.DateTimeField(default=timezone.now())
+    creation_date = models.DateTimeField('created date', default=None, blank=True, null=True)
 
     def __str__(self):
-        return self.name + " : " + self.tags
+        return self.name
 
     def was_created_recently(self):
         return self.creation_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Execution(models.Model):
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
-    execution_tags = models.CharField(max_length=200)
-    execution_status = models.CharField(max_length=50, default = "NOT RUN")
-    start_date = models.DateTimeField('date started', default=timezone.now())
-    finish_date = models.DateTimeField('date finished')
-
-    def __str__(self):
-        return self.tags + " : " + self.status
+    job_id = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
+    tags = models.CharField(max_length=200)
+    status = models.CharField(max_length=50, default = "NOT RUN")
+    start_date = models.DateTimeField('date started', default=None, blank=True, null=True)
+    finish_date = models.DateTimeField('date finished', default=None, blank=True, null=True)
