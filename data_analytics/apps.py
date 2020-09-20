b26087d35d88baf9927e8d5bca42df71ffbeda5b
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-20 11:36:43 +0200

    removed obsolete application

--- data_analytics/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataAnalyticsConfig(AppConfig):
-    name = 'data_analytics'
--- data_analytics/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-from . import views
-
-urlpatterns = [
-    path('data_analytics/', include('data_analytics.urls')),
-    path('admin/', admin.site.urls),
-]
