285f5f99494c0261ebeb963cdf6f7abed39515d7
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-30 18:23:16 +0200

    changed root name

--- data_analytics_service/urls.py
+++ data_analytics_service/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('analytics/', include('analytics.urls')),
-    path('admin/', admin.site.urls),
+    path('api/', include('analytics.urls')),
+    path('admin/', admin.site.urls)
 ]
