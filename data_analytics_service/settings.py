3f20360b1bf349efcd951dd48254917044f24aba
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-10 10:16:51 +0200

    added MongoDB connect setup

--- data_analytics_service/settings.py
+++ data_analytics_service/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'data_analytics_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'TCM',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
