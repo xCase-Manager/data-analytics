8b915ca2ca82845ee0f28d364b728f483886f90a
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-10 16:13:52 +0200

    added main view

---
+++ analytics/views/view.py
@@ -0,0 +1,30 @@
+from rest_framework import viewsets
+import json, pandas as pd
+
+
+class View(viewsets.ModelViewSet):
+    """
+    main view
+    """
+    def _filterBy(self, orm, key1, key2):
+        """
+        filtered by keys
+        """
+        return self._jsonResponse(self._getList(orm)
+            .set_index([key1, key2])
+            .count(level=key2)
+            .to_json())
+    
+    def _getList(self, orm):
+        """
+        list orm entries
+        """
+        return pd.DataFrame(orm)
+
+    def _jsonResponse(self, payload):
+        """
+        response formatter
+        """
+        return Response(
+            json.loads(payload), 
+            safe=False)
